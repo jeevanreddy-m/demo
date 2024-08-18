4bbbee824751b5eb8edd6d95c97461a606224257
g1reddy-m <g1reddy899@gmail.com>
2024-08-18 01:46:29 -0500

    Push terraform and github workflows files10

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,33 @@ terraform {
     key                  = "dev.terraform.tfstate"
   }
 }
-module "RG" {
-  source   = "./modules/RG" #A
-  rgname   = var.rgname     #B
-  location = var.location
+# module "RG" {
+#   source   = "./modules/RG" #A
+#   rgname   = var.rgname     #B
+#   location = var.location
+# }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "github-terraform-rg1"
+  location = "East US"
 }
-module "SA" {
-  source   = "./modules/StorageAccount"
-  sname    = var.sname
-  rgname   = var.rgname
-  location = var.location
+
+
+# module "SA" {
+#   source   = "./modules/StorageAccount"
+#   sname    = var.sname
+#   rgname   = var.rgname
+#   location = var.location
+# }
+
+resource "azurerm_storage_account" "STA" {
+  name                     = "storagegithubtf"
+  resource_group_name      = "github-terraform-rg1"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "dev"
+  }
 }
