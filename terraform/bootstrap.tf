e97da96ccc5c4f65466ee742c9562dce8391788b
g1reddy-m <g1reddy899@gmail.com>
2024-08-18 12:01:45 -0500

    Push terraform and github workflows files7

---
+++ terraform/bootstrap.tf
@@ -0,0 +1,22 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "github-terraform-rg1"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "storagegithubtf"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
