fa702b77495dcc1b0166d86d4f13887dff375796
g1reddy-m <g1reddy899@gmail.com>
2024-08-18 12:34:34 -0500

    Push terraform and github workflows files7

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.46.0"
+#     }
+#   }
+# }
+# provider "azurerm" {
+#   features {}
+# }
