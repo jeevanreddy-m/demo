caa481f5be48edfe306370b1db17d122141ad8f6
g1reddy-m <g1reddy899@gmail.com>
2024-08-25 13:12:16 -0500

    add ..

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.46.0"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "=2.46.0"
+#     }
+#   }
+# }
+# provider "azurerm" {
+#   features {}
+# }
