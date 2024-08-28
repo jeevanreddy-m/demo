89670015bc196499bb6f284c07505ee54d3ddf33
g1reddy-m <g1reddy899@gmail.com>
2024-08-27 21:25:17 -0500

    add providers in module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,19 @@
 # Configure the Azure Active Directory provider version and service principal credentials
-provider "azuread" {
+/*provider "azuread" {
   alias          = "ad"
   tenant_id = var.tenant_id
   client_id = var.client_id
   client_secret = var.client_secret
-}
+}*/
 # Configure the Azure Resource Manager provider version and service principal credentials
 provider "azurerm" {
-  alias          = "arm"
+  /*alias          = "arm"*/
   features {
   }
   tenant_id = var.tenant_id
   subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
+  /*client_id = var.client_id
+  client_secret = var.client_secret*/
 }
 /*# Configuring the Azure Resource Manager provider version and service principal credentials with different subscription
 provider "azurerm" {
@@ -25,9 +25,9 @@ provider "azurerm" {
   client_secret = var.client_secret
 }*/
 module "resource-group" {
-  providers = {
+  /*providers = {
     azurerm = azurerm.arm
-  }
+  }*/
   #source = "git@github.com:AAInternal/terraform.git//azure-modules/resource-group?ref=resource-group-v2.1.0"
   source   = "./modules/resource-group"
   /*aa-subscription-id = var.aa-subscription-id
